7729576668eb3608424839995031d73f2410ac96
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-08-28 15:50:42 -0300

    feat: showUsers method has been created

--- app/src/main/java/com/example/appfirebasejetpack/data/repository/FirebaseRepository.kt
+++ app/src/main/java/com/example/appfirebasejetpack/data/repository/FirebaseRepository.kt
@@ -1,7 +1,10 @@
 package com.example.appfirebasejetpack.data.repository
 
 import com.example.appfirebasejetpack.data.model.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 object FirebaseRepository {
     private val database = FirebaseDatabase.getInstance()
@@ -12,4 +15,19 @@ object FirebaseRepository {
         val usuario = User(id, nome, email, telefone, mensagem)
         usuariosRef.child(id).setValue(usuario)
     }
+
+    fun showUsers(onResult: (List<User>) -> Unit) {
+        usuariosRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lista = snapshot.children.mapNotNull { it.getValue(User::class.java) }
+                onResult(lista)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Erro: ${error.message}")
+            }
+        })
+    }
+
+
 }
