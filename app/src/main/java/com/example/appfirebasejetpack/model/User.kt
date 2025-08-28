f8e5adb35e1bb1dd7953d625ac28e76b263de444
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-08-28 15:24:47 -0300

    feat: model class User.kt has been created

---
+++ app/src/main/java/com/example/appfirebasejetpack/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.appfirebasejetpack.model
+
+data class User(
+    val id: String = "",
+    val nome: String = "",
+    val email: String = "",
+    val telefone: String = "",
+    val mensagem: String = ""
+)
