1c585b20158945196fdf6b552fe5ae8addaca876
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-09-09 16:36:43 -0300

    feat & fix: little changes in TelaCadastro.kt and TelaLogin.kt, and fixing a error in navigation

--- app/src/main/java/com/example/appfirebasejetpack/ui/screens/TelaCadastro.kt
+++ app/src/main/java/com/example/appfirebasejetpack/ui/screens/TelaCadastro.kt
@@ -1,15 +1,21 @@
 package com.example.appfirebasejetpack.ui.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -19,10 +25,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.appfirebasejetpack.ui.navigation.Routes
 import com.example.appfirebasejetpack.viewmodel.AuthState
 import com.example.appfirebasejetpack.viewmodel.AuthViewModel
@@ -57,13 +69,15 @@ fun TelaCadastro(
     }
 
     Column (
-        modifier = modifier.fillMaxSize(),
+        modifier
+            .fillMaxSize()
+            .background(Color(26,26,26)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Signup Page", fontSize = 32.sp)
+        Text(text = "Cadastre-se", fontSize = 32.sp, color = Color.Cyan)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier.height(16.dp))
 
         OutlinedTextField(
             value = email,
@@ -72,7 +86,26 @@ fun TelaCadastro(
             },
             label = {
                 Text(text = "Email")
-            }
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Cyan,
+                unfocusedLabelColor = Color.Cyan,
+                unfocusedContainerColor = Color(46,46,46),
+                unfocusedTextColor = Color.White,
+
+                focusedIndicatorColor = Color.Cyan,
+                focusedLabelColor = Color.Cyan,
+                focusedContainerColor = Color(60,60,60),
+                focusedTextColor = Color.White,
+
+                cursorColor = Color.Cyan
+
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                // Serve para quando der o enter ele ir para o próximo campo
+                imeAction = ImeAction.Next
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -84,25 +117,51 @@ fun TelaCadastro(
             },
             label = {
                 Text(text = "Senha")
-            }
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Cyan,
+                unfocusedLabelColor = Color.Cyan,
+                unfocusedContainerColor = Color(46,46,46),
+                unfocusedTextColor = Color.White,
+
+                focusedIndicatorColor = Color.Cyan,
+                focusedLabelColor = Color.Cyan,
+                focusedContainerColor = Color(60,60,60),
+                focusedTextColor = Color.White,
+
+                cursorColor = Color.Cyan
+
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier.height(16.dp))
 
-        Button(
+        OutlinedButton(
             onClick = {
                 authViewModel.signup(email, password)
-            }, enabled = authState.value != AuthState.Loading
+            },
+            enabled = authState.value != AuthState.Loading,
+            colors = ButtonColors(
+                containerColor = Color(26,26,26),
+                contentColor = Color.White,
+                disabledContentColor = Color.Cyan,
+                disabledContainerColor = Color(26,26,26)
+            ),
+            border = BorderStroke(1.dp, Color.Cyan)
         ) {
-            Text(text = "Criar Conta")
+            Text(text = "Criar Conta", color = Color.Cyan)
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier.height(8.dp))
 
         TextButton(onClick = {
-            navController.navigate(Routes.Dashboard)
+            navController.navigate(Routes.TelaLogin)
         }) {
-            Text(text = "Você já tem uma conta?")
+            Text(text = "Você já tem uma conta?", color = Color.Cyan)
         }
     }
 }
--- app/src/main/java/com/example/appfirebasejetpack/ui/screens/TelaLogin.kt
+++ app/src/main/java/com/example/appfirebasejetpack/ui/screens/TelaLogin.kt
@@ -1,15 +1,20 @@
 package com.example.appfirebasejetpack.ui.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -19,7 +24,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -55,11 +63,13 @@ fun TelaLogin(
     }
 
     Column (
-        modifier = modifier.fillMaxSize(),
+        modifier
+            .fillMaxSize()
+            .background(Color(26,26,26)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Login", fontSize = 32.sp)
+        Text(text = "Login", color = Color.Cyan, fontSize = 32.sp)
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -70,7 +80,26 @@ fun TelaLogin(
             },
             label = {
                 Text(text = "Email")
-            }
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Cyan,
+                unfocusedLabelColor = Color.Cyan,
+                unfocusedContainerColor = Color(46,46,46),
+                unfocusedTextColor = Color.White,
+
+                focusedIndicatorColor = Color.Cyan,
+                focusedLabelColor = Color.Cyan,
+                focusedContainerColor = Color(60,60,60),
+                focusedTextColor = Color.White,
+
+                cursorColor = Color.Cyan
+
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                // Serve para quando der o enter ele ir para o próximo campo
+                imeAction = ImeAction.Next
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -82,24 +111,49 @@ fun TelaLogin(
             },
             label = {
                 Text(text = "Senha")
-            }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Cyan,
+                unfocusedLabelColor = Color.Cyan,
+                unfocusedContainerColor = Color(46,46,46),
+                unfocusedTextColor = Color.White,
 
-        Button(onClick = {
+                focusedIndicatorColor = Color.Cyan,
+                focusedLabelColor = Color.Cyan,
+                focusedContainerColor = Color(60,60,60),
+                focusedTextColor = Color.White,
+
+                cursorColor = Color.Cyan
+
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
+        )
+        Spacer(modifier.height(16.dp))
+
+        OutlinedButton(onClick = {
             authViewModel.login(email, password)
         },
-            enabled = authState.value != AuthState.Loading
+            enabled = authState.value != AuthState.Loading,
+            colors = ButtonColors(
+                containerColor = Color(26,26,26),
+                contentColor = Color.White,
+                disabledContentColor = Color.Cyan,
+                disabledContainerColor = Color(26,26,26)
+            ),
+            border = BorderStroke(1.dp, Color.Cyan)
         ) {
-            Text(text = "Login")
+            Text(text = "Login", color = Color.Cyan)
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier.height(8.dp))
 
         TextButton(onClick = {
             navController.navigate(Routes.TelaCadastro)
         }) {
-            Text(text = "Você não tem uma conta? Cadastre-se")
+            Text(text = "Você não tem uma conta? Cadastre-se", color = Color.Cyan)
         }
     }
 }
